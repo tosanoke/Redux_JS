90b99db37ea9f92036b707059af3a2c92b1e3f65
tosanoke <tosanoke@gmail.com>
2022-08-27 22:11:52 +0100

    added cart items and reducers

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -5,7 +5,7 @@ import cartItems from '../../cartItems'
 
 const initialState = {
     cartItems: cartItems,
-    amount: 10,
+    amount: 0,
     total: 0,
     isLoading: true,
 }
@@ -17,7 +17,32 @@ export const cartSlice = createSlice({
         clearCart: (state) => {
           state.cartItems = [];
         },
+        removeItem: (state, action) => {
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+          },
+        increase: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount + 1;
+          },
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+          },
+        calculateTotals: (state) => {
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach((item) => {
+              amount += item.amount;
+              total += item.amount * item.price;
+            });
+            state.amount = amount;
+            state.total = total;
+          },
       },
 })
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
