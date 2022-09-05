d1fc2f109540125f26006059e7bbce8a0553946b
tosanoke <tosanoke@gmail.com>
2022-09-05 23:18:11 +0100

    added async thunk

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,7 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 import cartItems from '../../cartItems'
 
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  async (name, thunkAPI) => {
+    try {
+      // console.log(name);
+      // console.log(thunkAPI);
+      // console.log(thunkAPI.getState());
+      // thunkAPI.dispatch(openModal());
+      const resp = await axios(url);
 
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("something went wrong");
+    }
+  }
+);
 
 const initialState = {
     cartItems: cartItems,
@@ -39,6 +54,20 @@ export const cartSlice = createSlice({
             state.amount = amount;
             state.total = total;
           },
+      },
+      extraReducers: {
+        [getCartItems.pending]: (state) => {
+          state.isLoading = true;
+        },
+        [getCartItems.fulfilled]: (state, action) => {
+          // console.log(action);
+          state.isLoading = false;
+          state.cartItems = action.payload;
+        },
+        [getCartItems.rejected]: (state, action) => {
+          console.log(action);
+          state.isLoading = false;
+        },
       },
 })
 
